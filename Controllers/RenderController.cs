00221a643ef10952bf5784d9125616ed284d1e9f
Vincent Clyde Burgos <vburgos@lemongrassconsulting.com>
2020-07-24 13:14:06 +0800

    init commit

---
+++ Controllers/RenderController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Syncfusion.Pdf;
+using Syncfusion.Pdf.Graphics;
+using Syncfusion.Drawing;
+
+
+namespace Syncfusion.PDF.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class RenderController : ControllerBase
+    {
+        private readonly ILogger<RenderController> _logger;
+
+        public RenderController(ILogger<RenderController> logger)
+        {
+            _logger = logger;
+        }
+        [HttpGet]
+        [Route("pdf")]
+        public async Task<IActionResult> RenderPDF()
+        {
+            //Create a new PDF document.
+            PdfDocument doc = new PdfDocument();
+            //Add a page to the document.
+            PdfPage page = doc.Pages.Add();
+            //Create PDF graphics for the page
+            PdfGraphics graphics = page.Graphics;
+            //Load the image as stream.
+            FileStream imageStream = new FileStream("test.png", FileMode.Open, FileAccess.Read);
+            PdfBitmap image = new PdfBitmap(imageStream);
+            //Draw the image
+            graphics.DrawImage(image, 0, 0);
+            //Save the PDF document to stream
+            MemoryStream stream = new MemoryStream();
+            doc.Save(stream);
+            //If the position is not set to '0' then the PDF will be empty.
+            stream.Position = 0;
+            //Close the document.
+            doc.Close(true);
+            //Defining the ContentType for pdf file.
+            string contentType = "application/pdf";
+            //Define the file name.
+            string fileName = "Output.pdf";
+            //Creates a FileContentResult object by using the file contents, content type, and file name.
+            return File(stream, contentType, fileName);
+        }
+    }
+}
